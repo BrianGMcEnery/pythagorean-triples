679006d5ffe07046c234049b305dd823b8cfb8be
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2021-02-10 14:18:23 +0000

    Playing with drawing triangles

--- exp/pygeom/triangle.py
+++ exp/pygeom/triangle.py
@@ -1,4 +1,4 @@
-from pygeom import Axes, Point, Triangle
+from pygeom import Axes, Point, Triangle, Vector
 
 # Create the cartesian axis
 axes = Axes(xlim=(-1,6), ylim=(-1,6), figsize=(8,6))
@@ -7,9 +7,12 @@ axes = Axes(xlim=(-1,6), ylim=(-1,6), figsize=(8,6))
 p1 = Point(0, 0, color='grey')
 p2 = Point(3, 0, color='grey')
 p3 = Point(3, 4, color='grey')
+p4 = Point(1.5, 3)
 
 tr = Triangle(p1, p2, p3, alpha=0.5)
+v = Vector(p1, p4)
 
-#axes.addMany([p1, p2, p3])
+axes.addMany([p1, p2, p3])
 axes.add(tr)
+axes.add(v)
 axes.draw()
